865cf795357e058031e21dbf71b1e9bc0d7ed5d4
LakshayJain458 <aalvpbjain93@gmail.com>
2024-05-24 01:04:19 +0530

    context api learning

---
+++ ContextApi_1/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+import UserContextProvider from './context/UserContextProvider'
+import Login from './components/Login'
+import Profile from './components/Profile'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <UserContextProvider>
+     <h1>Hello World</h1> 
+     <Login/>
+     <Profile/>
+    </UserContextProvider>
+    
+  )
+}
+
+export default App
